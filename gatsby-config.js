9e009e4bd732b558921f927ab9e9a0ad591521ba
niko-bjs <schmidt@makebasic.com>
2020-08-25 15:25:26 +0200

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby and WordPress`,
+    subtitle: `Fetch Data!`,
+    description: `Oh Yea!!`,
+    author: `@makebasic`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-react-helmet',
+    {
+        resolve: "gatsby-source-wordpress",
+        options: {
+            baseUrl: "localhost:8888/wp-react/",
+            protocol: "http",
+            hostingWPCOM: false,
+            useACF: true,
+            verboseOutput: true
+        }
+    }
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
